fc2580f740a8630f8b43788c0e4cb2efa66327a5
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2025-10-07 04:19:02 -0300

    Exemplo

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int n1 = 89;
@@ -5,5 +7,18 @@ public class Main {
         System.out.println(n1 & n2);
         System.out.println(n1 | n2);
         System.out.println(n1 ^ n2);
+
+        // --- outro teste ---
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int mask = 0b100000; //32 em bit
+
+        if((n & mask) != 0) {
+            System.out.println("6th bit is true!");
+        } else {
+            System.out.println("6th bit is false!");
+        }
     }
 }
